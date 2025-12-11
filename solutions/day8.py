a97a3352ce773f00e3778739dfb90b2ef5ef9919
Lukas Adie <lukas@lukadd.io>
2025-12-10 20:58:55 -0500

    fix: day 8 part 1
    The question explicitly says connect the 1000 nearest pairs :man_facepalming:

--- solutions/day8.py
+++ solutions/day8.py
@@ -4,29 +4,10 @@
 # Each position is given as x,y,z coordinates
 # The goal is to connect pairs of junction boxes that are as close together as possible
 
-# PART 1: What do you get if you multiply together the sizes (number of junction boxes) of the three largest circuits?
+# PART 1: Connect together the 1000 pairs of junction boxes which are closest together; what do you get if you multiply together the sizes (number of junction boxes) of the three largest circuits?
 # PART 2: tbd
 
 import math
-
-
-# Given a point and a list of other points, find its nearest neighbour
-def closest_from_coordinates(
-    point: tuple[int, int, int], others: list[tuple[int, int, int]]
-) -> tuple[int, int, int]:
-    # Use squared distance to find closest pairs
-    best_d2 = math.inf
-    best_pt = None
-    px, py, pz = point
-    for x, y, z in others:
-        dx = px - x
-        dy = py - y
-        dz = pz - z
-        d2 = dx * dx + dy * dy + dz * dz
-        if d2 < best_d2:
-            best_d2 = d2
-            best_pt = (x, y, z)
-    return best_pt
 
 
 def run() -> None:
@@ -55,9 +36,7 @@ def run() -> None:
     # Sort pairs by squared distance
     pairs.sort(key=lambda x: x[0])
 
-    # print(len(pairs))
-
-    top_n = len(positions) // 2
+    top_n = 1000
     print(len(positions), top_n)
     print(f"Top {top_n} shortest connections (squared distance, distance, pair):")
     for d2, (a, b) in pairs[:top_n]:
@@ -88,25 +67,6 @@ def run() -> None:
             for idx in sorted(indices[1:], reverse=True):
                 circuits[first].update(circuits[idx])
                 del circuits[idx]
-
-        # # If one of the junction boxes is already in a circuit, add the other to that same circuit
-        # # If both junction boxes are already in the same circuit, do nothing
-        # # Else, form a new circuit with both junction boxes
-        # found_existing = False
-        # for circuit in circuits:
-        #     if (junc_a in circuit) and (junc_b in circuit):
-        #         found_existing = True
-        #         print(junc_a, junc_b, f"--> already in a circuit: {circuit}")
-        #         break
-        #     if (junc_a in circuit) or (junc_b in circuit):
-        #         found_existing = True
-        #         print(junc_a, junc_b, f"--> adding to existing circuit: {circuit}")
-        #         circuit.add(junc_a)
-        #         circuit.add(junc_b)
-        #         break
-        # if not found_existing:
-        #     print(junc_a, junc_b, "--> adding to new circuit")
-        #     circuits.append({junc_a, junc_b})
 
     # print(circuits)
     print(len(circuits))
